00e2214f1d81ec5ccd85fead7caa344b8b1da63e
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-13 12:32:45 +0330

    increase iframe height + push buttons downer

--- src/components/atoms/Gamefram.tsx
+++ src/components/atoms/Gamefram.tsx
@@ -10,11 +10,11 @@ const GameFram: FC<GameframPropsType> = ({
 
   const maxWidth = 600;
   const width = 1800;
-  const height = 1350;
+  const height = 1350 * 3 / 2;
   const scaleRate = maxWidth / width;
 
   return (
-    <iframe allow="camera;microphone" src={src} width={width} height={height} style={{ position: 'absolute', top: 120, border: 0, borderRadius: 5, transform: `translate(0%, -${scaleRate / 2 * 100}%) scale(${scaleRate})` }} />
+    <iframe allow="camera;microphone" src={src} width={width} height={height} style={{ position: 'absolute', top: 0, border: 0, borderRadius: 5, transform: `translate(0%, -${scaleRate / 2 * 100}%) scale(${scaleRate})` }} />
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,11 @@ function Home() {
       <StepLayout step={StepsData[currentStepIndex]} />
       <script type="text/javascript" src={`${basePath}/js/impress.js`} />
       <IconButton disableRipple onClick={() => setCurrentStepIndex(currentStepIndex => (currentStepIndex - 1 + StepsDataSize) % StepsDataSize)}
-        sx={{ position: 'fixed', bottom: 180, left: 0, width: 100 }}>
+        sx={{ position: 'fixed', bottom: 120, left: 0, width: 100 }}>
         <Image src={PreviousButton} width={0} alt="" style={{ width: '100%' }} />
       </IconButton>
       <IconButton disableRipple onClick={() => setCurrentStepIndex(currentStepIndex => (currentStepIndex + 1) % StepsDataSize)}
-        sx={{ position: 'fixed', bottom: 180, right: 0, width: 100 }}>
+        sx={{ position: 'fixed', bottom: 120, right: 0, width: 100 }}>
         <Image src={NextButton} width={0} alt="" style={{ width: '100%' }} />
       </IconButton>
       <Image src={ICMLogo} alt="" width={0} style={{ position: 'fixed', bottom: 10, right: 10, width: 30 }} />
