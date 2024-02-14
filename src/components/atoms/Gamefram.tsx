70203a27b3f4f2ca9e5d643208e5fbcea8577090
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-02-14 21:24:40 +0330

    decrease the iframe top position

--- src/components/atoms/Gamefram.tsx
+++ src/components/atoms/Gamefram.tsx
@@ -14,7 +14,7 @@ const GameFram: FC<GameframPropsType> = ({
   const scaleRate = maxWidth / width;
 
   return (
-    <iframe allow="camera;microphone" src={src} width={width} height={height} style={{ position: 'absolute', top: 20, border: 0, borderRadius: 5, transform: `translate(0%, -${scaleRate / 2 * 100}%) scale(${scaleRate})` }} />
+    <iframe allow="camera;microphone" src={src} width={width} height={height} style={{ position: 'absolute', top: 10, border: 0, borderRadius: 5, transform: `translate(0%, -${scaleRate / 2 * 100}%) scale(${scaleRate})` }} />
   )
 }
 
